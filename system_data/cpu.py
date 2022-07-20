2905f3d384ef9d29989294bca78014483d367724
MarcusVMendes <marcus.mendes89@gmail.com>
2022-07-20 14:33:53 -0300

    feat: insert cpu_percent per cpu on interface

--- system_data/cpu.py
+++ system_data/cpu.py
@@ -27,6 +27,20 @@ def apend_total_cpu_percent_usage_to_interface():
     interface.title = f'Total CPU usage: {data}%'
 
 
+def get_percentage_of_cpu_per_core():
+    cpu_percent_per_core = cpu_percent(percpu=True)
+    return cpu_percent_per_core
+
+
+def apend_percentage_of_cpu_per_core_to_interface():
+    data = get_percentage_of_cpu_per_core()
+    interface = ui.items[1].items[1:5]
+    for index, (percent, gauge) in enumerate(zip(data, interface)):
+        gauge.value = percent
+        gauge.title = f'CORE-{index + 1}: {percent}%'
+
+
 def cpu_module():
     apend_cpu_freq_to_interface()
     apend_total_cpu_percent_usage_to_interface()
+    apend_percentage_of_cpu_per_core_to_interface()
