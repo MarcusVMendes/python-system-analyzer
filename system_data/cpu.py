207ac7a8f6c45829e85a539f93cc9542c3fe0d88
MarcusVMendes <marcus.mendes89@gmail.com>
2022-07-20 13:53:57 -0300

    feat: insert cpu freq in dashboard

--- system_data/cpu.py
+++ system_data/cpu.py
@@ -1,11 +1,19 @@
-from psutil import cpu_freq
 from dashboard.interface import ui
+from psutil import cpu_freq
+from utils.converters import convert_mhz_to_ghz
 
 
 def get_cpu_freq():
-    get_cpu_freq = cpu_freq()
-    print(get_cpu_freq)
+    get_cpu_freq_mhz = cpu_freq().current
+    freq_ghz = convert_mhz_to_ghz(get_cpu_freq_mhz)
+    return freq_ghz
+
+
+def apend_cpu_freq_to_interface():
+    data = get_cpu_freq()
+    interface = ui.items[1]
+    interface.title = f'CPU Freq: {data:.2f} Ghz'
 
 
 def cpu_module():
-    get_cpu_freq()
+    apend_cpu_freq_to_interface()
---
+++ utils/converters.py
@@ -0,0 +1,3 @@
+def convert_mhz_to_ghz(freq):
+    ghz = freq / 1000
+    return ghz
