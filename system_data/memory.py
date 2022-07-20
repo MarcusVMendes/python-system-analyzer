556b502bd9192dea45d0550d287c7de1777360e7
MarcusVMendes <marcus.mendes89@gmail.com>
2022-07-20 18:33:38 -0300

    feat: insert RAM percent in VGauge interface

--- system_data/memory.py
+++ system_data/memory.py
@@ -17,7 +17,7 @@ def apend_virtual_memory_infos_to_interface():
     used = convert_bytes_to_gigas(data.used)
     percent = data.percent
 
-    interface.text += "\n{:>4}{:>10.2f}{:>10.2f}{:>8}".format(
+    interface.text += "\n{:>4}{:>10.2f}{:>10.2f}{:>8}\n".format(
         '',
         available,
         used,
@@ -33,7 +33,9 @@ def get_swap_memory_infos():
 def apend_swap_memory_infos_to_interface():
     data = get_swap_memory_infos()
     interface = ui.items[0].items[1].items[0].items[0]
-    interface.text += f"\n{'SWAP':>4}{'AVAILABLE(GB)':>14}{'USED(GB)':>9}{'%':>4}"
+    interface.text += (
+        f"{'SWAP':>4}{'AVAILABLE(GB)':>14}{'USED(GB)':>9}{'%':>4}"
+    )
 
     swap_available = swap_memory().total - swap_memory().used
     available = convert_bytes_to_gigas(swap_available)
@@ -48,6 +50,14 @@ def apend_swap_memory_infos_to_interface():
     )
 
 
+def apend_percent_virtual_memory_to_interface():
+    data = get_virtual_memory_infos()
+    interface = interface = ui.items[0].items[1].items[0].items[1].items[0]
+    percent = data.percent
+    interface.value = percent
+
+
 def memory_module():
     apend_virtual_memory_infos_to_interface()
     apend_swap_memory_infos_to_interface()
+    apend_percent_virtual_memory_to_interface()
